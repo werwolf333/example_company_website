05b91474fed8378edd17c7ed1599079d92a166bd
Werwolf <slmig@mail.ru>
2020-02-27 23:40:49 +0300

    add registration in view login_and_registration
    add class:
     - registration (RegistrationFormView)
     in views
     in the login_and_registration app

--- login_and_registration/views.py
+++ login_and_registration/views.py
@@ -1,9 +1,16 @@
-from django.views.generic import FormView
+from django.views.generic.edit import FormView
 from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
 
 
 class RegistrationFormView(FormView):
-    pass
+    form_class = UserCreationForm
+    template_name = "login_and_registration/registration.html"
+    success_url = "/login/?registration_completed"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
 class LoginFormView(FormView):
