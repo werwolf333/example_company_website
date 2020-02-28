46cafad68b54f329a0a808450f26fa1fc0b6a946
Werwolf <slmig@mail.ru>
2020-02-28 16:59:03 +0300

    add Registration in urls login_and_registration
    add urls RegistrationFormView
    in urls
    in the login_and_registration app
    add stub (RegistrationFormView class)
    in view
    in the login_and_registration app

--- login_and_registration/urls.py
+++ login_and_registration/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('registration/', views.RegistrationFormView.as_view(), name='Registration'),
 ]
--- login_and_registration/views.py
+++ login_and_registration/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import FormView
 
-# Create your views here.
+
+class RegistrationFormView(FormView):
+    pass
