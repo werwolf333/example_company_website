55d1b6770b5618f0dcb6f0619286723432d13cc4
Werwolf <slmig@mail.ru>
2020-03-01 20:40:56 +0300

    add People in models people
    add class People
    in models
    in the people app

--- people/models.py
+++ people/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class People(models.Model):
+    name = models.CharField(max_length=20, verbose_name="имя")
+    surname = models.CharField(max_length=20, verbose_name="фамилия")
+    patronymic = models.CharField(max_length=20, verbose_name="Отчество")
+    position = models.CharField(max_length=20, verbose_name="Должность")
+    boss = models.ForeignKey("self", blank=True, null=True, related_name='workers', on_delete=models.DO_NOTHING)
+    image = models.ImageField(blank=True, upload_to='images/people/', help_text='100x100px', verbose_name='ссылка картинки')
+
+    def __str__(self):
+        return self.position + ': '+self.surname + ' '+self.name +' '+self.patronymic
+
+    def full_name(self):
+        return self.surname + ' '+self.name +' '+self.patronymic
