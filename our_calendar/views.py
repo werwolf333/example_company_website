b66b204b65367ead3d8aa3a98ea0b9f6002c77f9
Werwolf <slmig@mail.ru>
2020-04-21 19:51:26 +0300

    add Index in view our_calendar
    add class:
    - Index
    in views
    in the our_calendar app

--- our_calendar/views.py
+++ our_calendar/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
+from our_calendar.models import OurCalendar
+from django.contrib import auth
 
 
 class Index(View):
-    pass
+
+    def get(self, request):
+        args = {}
+        args['dates'] = OurCalendar.objects.all()
+        args['username'] = auth.get_user(request).username
+        return render(request, "our_calendar/our_calendar_list.html", args)
 
 
 class HolidayAdd(View):
