19f671db5d08cc90454a41006ede4db1b5e484b6
Werwolf <slmig@mail.ru>
2020-04-21 19:06:07 +0300

    add OurCalendar in models our_calendar
    add class OurCalendar
    in models
    in the our_calendar app

--- our_calendar/models.py
+++ our_calendar/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from people.models import People
 
-# Create your models here.
+
+
+class OurCalendar(models.Model):
+    man = models.ForeignKey(People, related_name='our_calendars', on_delete=models.DO_NOTHING)
+    data_start = models.DateField(null=True, blank=True)
+    data_finish = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.man)
+
+    def res(self):
+        return self.data_finish - self.data_start
