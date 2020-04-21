273c0ddbb3f09ed7cd278398bedf9748f34f5688
Werwolf <slmig@mail.ru>
2020-04-21 18:49:38 +0300

    add static and media in urls our_calendar
    add urls (static and media)
    in urls
    in the our_calendar app

--- our_calendar/urls.py
+++ our_calendar/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.Index.as_view(), name='Index'),
@@ -7,3 +9,6 @@ urlpatterns = [
     path('edit/<int:id>', views.HolidayEdit.as_view(), name='HolidayEdit'),
     path('delete/<int:id>', views.HolidayDelete.as_view(), name='HolidayDelete'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
